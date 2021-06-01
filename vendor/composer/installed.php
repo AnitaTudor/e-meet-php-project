168d05ccb30933169a83fbcea4697e33b48a364b
Anita Tudor Alexandru <tudoranita1054@gmail.com>
2021-06-01 17:27:16 +0300

    composer lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '2b7dbf73ecff347513dd4301fb55ba4056cb09e1',
+    'name' => 'anitatudor/e_meet',
+  ),
+  'versions' => 
+  array (
+    'anitatudor/e_meet' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2b7dbf73ecff347513dd4301fb55ba4056cb09e1',
+    ),
+  ),
+);
